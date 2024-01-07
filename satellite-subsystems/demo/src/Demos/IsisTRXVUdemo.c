d99d5a2b33e2ed04b558fc1c18659e760b398696
urisinger <urisig2007@gmail.com>
2024-01-07 19:41:57 +0200

    changed function names to be more consistent

--- satellite-subsystems/demo/src/Demos/IsisTRXVUdemo.c
+++ satellite-subsystems/demo/src/Demos/IsisTRXVUdemo.c
@@ -95,7 +95,7 @@ static Boolean vutc_sendDefClSignTest(void)
 }
 
 
-static Boolean Packets_Lunch_By_User_Input(void)
+static Boolean packetsLunchByUserInput(void)
 {
 	//Buffers and variables definition
 	unsigned char txCounter = 'a';
@@ -516,30 +516,30 @@ static Boolean vutc_getTxTelemTest_revD(void)
 	return TRUE;
 }
 
-static Boolean TurnOnTransponder(void){
+static Boolean turnOnTransponder(void){
 	unsigned char turn_on_cmd[]={0x38,2};
 		I2C_write(0x61,turn_on_cmd,2);
 		return TRUE;
 }
 
-static Boolean TurnOffTransponder(void){
+static Boolean turnOffTransponder(void){
 	unsigned char turn_off_cmd[]={0x38,1};
 		I2C_write(0x61,turn_off_cmd,2);
 		return TRUE;
 }
 
-static Boolean TurnOnTransponderWithDelay(void)
+static Boolean turnOnTransponderWithDelay(void)
 {
 	int time_in_min = 0;
 	while(UTIL_DbguGetIntegerMinMax(&time_in_min, 1, 20) == 0);
 
-	if(!TurnOnTransponder() == FALSE){
+	if(!turnOnTransponder() == FALSE){
 		return FALSE;
 	}
 
 	vTaskDelay (time_in_min*(60000/portTICK_RATE_MS));
 
-	if(!TurnOffTransponder() == FALSE){
+	if(!turnOffTransponder() == FALSE){
 		return FALSE;
 	}
 	return TRUE;
@@ -614,16 +614,16 @@ static Boolean selectAndExecuteTRXVUDemoTest(void)
 		offerMoreTests = vutc_sendInputTest();
 		break;
 	case 14:
-		offerMoreTests = TurnOnTransponderWithDelay();
+		offerMoreTests = turnOnTransponderWithDelay();
 		break;
 	case 15:
-		offerMoreTests = TurnOnTransponder();
+		offerMoreTests = turnOnTransponder();
 		break;
 	case 16:
-		offerMoreTests = TurnOffTransponder();
+		offerMoreTests = turnOffTransponder();
 		break;
 	case 17:
-		offerMoreTests = Packets_Lunch_By_User_Input();
+		offerMoreTests = packetsLunchByUserInput();
 		break;
 	case 18:
 		offerMoreTests = FALSE;
