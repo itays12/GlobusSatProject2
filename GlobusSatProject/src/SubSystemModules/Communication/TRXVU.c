013f5023ee9c89213a59660b4642dfb000e539a3
ShakedDrai <s.shakedderi@sng-edu.org>
2024-06-18 15:15:31 +0300

    TRX_Logic , CheckExecutionTime
    we Workd on the TRX_Logic & CheckExecutionTime Approved by Uri &Adi

--- GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
+++ GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
@@ -35,3 +35,54 @@ int TRXVUInit(void)
 
 	return rv;
 }
+////////////////////new code
+//////////Approved by Adi
+int TRX_Logic(){
+	int frame_count=GetNumberOfFramesInBuffer();
+	if (frame_count>0){
+
+	}
+	return 1;
+}
+int GetNumberOfFramesInBuffer(){
+	unsigned short frame_count = 0;
+	int error =IsisTrxvu_rcGetFrameCount(0, &frame_count);
+	logError(error , "error in get frame count");
+	return frame_count;
+}
+//////////
+/////////code in process
+int ActUponCommand(sat_packet_t *cmd){
+
+}
+int BeaconLogic(Boolean forceTX){
+	time_unix period = 60;//An educated guess, slide 7
+
+	if( CheckExecutionTime( g_beacon_interval_time, period)){
+		GetCurrentWODTelemetry(WOD_Telemetry_t *wod);
+	}
+
+}
+//////////Approved by Uri
+Boolean CheckExecutionTime(time_unix prev_time, time_unix period){
+	Time cur_t_Time;
+	cur_t_Time =Time_get( &cur_t_Time);
+
+	logError(cur_t_Time,"error in CheckExecutionTime Time_get");
+
+	time_unix cur_t_time_unix = Time_convertTimeToEpoch(&cur_t_time_unix);
+
+	if(prev_time+period>=cur_t_time_unix)
+		return TRUE;
+	else return FALSE;
+}
+/////
+void GetCurrentWODTelemetry(WOD_Telemetry_t *wod){
+
+
+}
+int AssembleCommand(unsigned char *data, unsigned short data_length, char type, char subtype,unsigned int id, sat_packet_t *cmd);
+{
+
+}
+//////
