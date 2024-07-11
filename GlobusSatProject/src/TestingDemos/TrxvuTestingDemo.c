1120bc3ff6a1daa35ec1e954f334cffaf38a87aa
ShakedDrai <s.shakedderi@sng-edu.org>
2024-07-11 12:20:19 +0300

    Create TrxvuTestingDemo.c
    add test

---
+++ GlobusSatProject/src/TestingDemos/TrxvuTestingDemo.c
@@ -0,0 +1,87 @@
+#include "TrxvuTestingDemo.h"
+#include "hal/Utility/util.h"
+#include "stdio.h"
+Boolean MainTrxvuTestBench(){
+	int selection = 0;
+		Boolean offerMoreTests = TRUE;
+		printf( "\n\r Select a test to perform: \n\r");
+		printf("\t 1)  Beacon Logic\n\r");
+		printf("\t 2) TRX Logic \n\r");
+		printf("\t 3) Transmit Test\n\r");
+
+
+	while(UTIL_DbguGetIntegerMinMax(&selection, 1, 19) == 0);
+
+		switch(selection) {
+		case 1:
+			offerMoreTests = BeaconLogicTest();
+			break;
+		case 2:
+			offerMoreTests = TRX_LogicTest();
+			break;
+		case 3:
+			offerMoreTests = TransmitTest();
+			break;
+		}
+}
+
+Boolean BeaconLogicTest(){
+	int WaitTime;
+	printf( "\n\r Select seconds to wait for Beacon Logic Test: \n\r");
+	while(UTIL_DbguGetIntegerMinMax(& WaitTime, 1, 121) == 0);
+	time_unix prev_time;
+	Time_getUnixEpoch(&prev_time);
+
+	while( CheckExecutionTime( prev_time,  WaitTime)){
+		BeaconLogic(TRUE);
+	}
+}
+Boolean  TRX_LogicTest(){
+	int WaitTime;
+	printf( "\n\r Select seconds to wait for Beacon Logic Test: \n\r");
+	while(UTIL_DbguGetIntegerMinMax(& WaitTime, 1, 121) == 0);
+	time_unix prev_time;
+	Time_getUnixEpoch(&prev_time);
+
+	while( CheckExecutionTime( prev_time,  WaitTime)){
+		 TRX_Logic(TRUE);
+	}
+}
+Boolean TransmitTest(){
+	//Buffers and variables definition
+		unsigned char testBuffer1[10]  = { 0 };
+		unsigned char txCounter = 0;
+		unsigned char avalFrames = 0;
+		unsigned int timeoutCounter = 0;
+
+		sat_packet_t packet;
+		printf( "\n\r enter ID: \n\r");
+			while(UTIL_DbguGetIntegerMinMax(&packet.ID) == 0);
+
+		printf( "\n\r enter subtype: \n\r");
+			while(UTIL_DbguGetIntegerMinMax(&packet.cmd_subtype) == 0);
+
+		printf( "\n\r enter type: \n\r");
+			while(UTIL_DbguGetIntegerMinMax(&packet.cmd_type) == 0);
+
+
+	    int i;
+	    unsigned int temp;
+
+	    for(i = 0; i < 10; i++){
+	    	if(UTIL_DbguGetHexa32(&temp)){
+	    		testBuffer1[i] = (unsigned char)(temp);
+	    	}
+	    	else
+	    	{
+	    		printf("/r/n Invalid input. Please enter a valid hexadecimal number.\r\n");
+	    		i=i-1;
+	    	}
+	    }
+
+	    TransmitSplPacket( &packet, &avalFrames);
+
+}
+
+
+
