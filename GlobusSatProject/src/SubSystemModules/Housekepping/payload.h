62028faced4d8e9fd6fb3c084c185e84bde9909d
storm <storm@fatman.lan>
2024-09-11 16:03:37 +0300

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit f259cdfb40c66fe9c68bff1d7de6b892d206709c, reversing
    changes made to e5dddb2cdd29a25a2e926bcd544b61745fe6170d.

--- GlobusSatProject/src/SubSystemModules/Housekepping/payload.h
+++ GlobusSatProject/src/SubSystemModules/Housekepping/payload.h
@@ -1,100 +1,18 @@
-/*
- * IsisTRXVUdemo.h
- *
- *  Created on: 6 feb. 2015
- *      Author: malv
- */
+#define SOREQ-I2C_ADDRESS 0x55
+#define READ_PIC32_RESET 0x66
+#define READ_DELAY 200
+#define TIMEOUT 4000
+#define READ_PIC32_RESETS 0x66
+#define PAYLOAD_I2C_ADDRESS
+#define CLEAR_WDT_AND_ALLOW_READ 0x45
+typedef enum {
+	PAYLOAD_SUCCESS, PAYLOAD_I2C_WRITE_Error, PAYLOAD_I2C_READ_Error, PAYLOAD_TIMEOUT
+}SoreqResult;
+typedef enum
+{
+	board, radfet,ormad,fpga
+}PayloadSubSystem;
 
-#ifndef ISIS_SOREQ_PAYLOAD_H_
-#define ISIS_SOREQ_PAYLOAD_H_
-
-#include <hal/boolean.h>
-
-/***
- * Starts demo.
- * Calls Init and Menu in sequence.
- * Returns FALSE on failure to initialize.
- */
-
-typedef struct {
-    unsigned char board;
-    unsigned char radfet1;
-    unsigned char radfet2;
-    unsigned char ormad;
-    unsigned char fpga;
-} RadiationData;
-
-Boolean MasterRadiationWrite(RadiationData* data);
-/***
- * This function reads the voltages for RADFET1 and RADFET2 from the Soreq payload.
- * OPCODE sent to the payload is 0x33.
- * Returns FALSE on failure.
- */
-
-Boolean MasterTemperatureWrite(void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0x77.
- * Returns FALSE on failure.
- */
-
-Boolean MasterPIC32SELWrite(void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0x66.
- * Returns FALSE on failure.
- */
-
-Boolean MasterPIC32SEUWrite(void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0x47.
- * Returns FALSE on failure.
- */
-
-Boolean softReset(void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0xF8.
- * Returns FALSE on failure.
- */
-
-Boolean clearWatchdog (void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0x3F.
- * Returns FALSE on failure.
- */
-
-Boolean soreqDebugging(void);
-/***
- * This function sends a command to the Soreq payload to start the radiation experiment.
- * OPCODE sent to the payload is 0x32.
- * Returns FALSE on failure.
- */
-
-Boolean soreqPayloadInit(void);
-
-/***
- * Loop producing an interactive
- * text menu for invoking subsystem functions
- * note:
- * Depends on an initialized TRXVU subsystem driver.
- */
-
-Boolean IsisTRXVUdemoMain(void);
-
-/***
- * Initializes the TRXVU subsystem driver.
- * Returns FALSE on failure.
- *
- * note:
- * Depends on an initialized I2C driver.
- * Initialize the I2C interface once before using
- * any of the subsystem library drivers
- */
-Boolean IsisTRXVUdemoInit(void);
-
-
-
-#endif /* ISIS_SOREQ_PAYLOAD_H_ */
+SoreqResult payloadSendCommand(char opcode,int size,unsigned char* buffer,int delay);
+SoreqResult payloadRead(int size, unsigned char* buffer);
+SoreqResult payloadPICread();
