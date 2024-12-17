5b6ae628dcaefb89fda488d13d4f29472fd7dee5
urisinger <urisig2007@gmail.com>
2024-12-17 22:12:03 +0200

    change spl packets and add MIB.json

--- GlobusSatProject/src/FRAM_FlightParameters.h
+++ GlobusSatProject/src/FRAM_FlightParameters.h
@@ -31,6 +31,7 @@ typedef struct FileSystemError{
 typedef struct FramFlightParams{
 	Boolean trxMute;
 	unsigned int trxMuteTime;
+	unsigned int telePeriod_trx;
 
 
 	unsigned int beaconInterval;
@@ -42,13 +43,6 @@ typedef struct FramFlightParams{
 	unsigned int telePeriod_eps;
 	unsigned int telePeriod_ant;
 	unsigned int telePeriod_wod;
-
-	unsigned int telePeriod_solar;
-	unsigned int telePeriod_trx;
-
-	unsigned int lastSavePayLoadTime15min = 0;
-	unsigned int lastSavePayLoadTime20sec = 0;
-
 	EpsModeVolts epsModes;
   
   FileSystemError fsError;
